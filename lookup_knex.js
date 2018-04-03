e4ce4c05f39a6464264a3cdffc8f79c3bfd140a5
Andy <voandy1998@gmail.com>
2018-04-03 21:54:33 +0000

    can lookup famous people using knex

---
+++ lookup_knex.js
@@ -0,0 +1,35 @@
+const settings = require("./settings");
+const pg = require("pg");
+
+const knex = require("knex")({
+  client: 'pg',
+  connection:  {
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+  }
+});
+
+const name = [process.argv[2]];
+
+knex.select().from('famous_people')
+.where('first_name', `${name}`)
+.orWhere('last_name', `${name}`)
+.asCallback(function(err, rows) {
+  if (err) return console.error(err);
+  let count = 1;
+  console.log("Searching ...");
+  console.log("Found " + rows.length + " person(s) by the name '" + name + "':");
+  rows.forEach(function(e) {
+  let birthday = e.birthdate.toISOString().slice(0,10);
+  console.log("-" + count + ": " + e.first_name + " " + e.last_name + ", born " + "'" + birthday + "'");
+  count++;
+  })
+  process.exit();
+})
+
+
+
